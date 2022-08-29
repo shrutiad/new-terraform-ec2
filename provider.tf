7739d4455a2b66100ee3115aff223680222c529a
shrutiad <109783667+shrutiad@users.noreply.github.com>
2022-08-29 23:32:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
-  region                  = "ap-south-1"
-  shared_credentials_file = "/Users/lenovo/.aws/creds" ####Aad Your path here#####
+  region                  = "ap-southeast-1"
+
 }
