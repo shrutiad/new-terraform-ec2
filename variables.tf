e4dfba578a0f7d220c432470c758b46e54d579ce
shrutiad <109783667+shrutiad@users.noreply.github.com>
2022-08-29 23:34:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,34 +5,7 @@ variable "instance_type" {
 
 variable "ami_id" {
   type    = string
-  default = "ami-052cef05d01020f1d"
+  default = "ami-0e64281826ea08b9e"
 }
 
-variable "volume_type" {
-  type    = string
-  default = "gp2"
-}
 
-variable "volume_size" {
-  type    = string
-  default = "10"
-}
-
-variable "ec2_key_name" {
-  type    = string
-  default = "terraform"
-}
-
-variable "subnet_name" {
-  type    = string
-  default = "subnet-6f19ee04"
-}
-
-variable "tag_values" {
-  type = map(any)
-  default = {
-    "Name"             = "Linux_Terraform_Demo",
-    "Created_By"       = "Terraform_Automation",
-    "Application_Name" = "Terraform_Demo"
-  }
-}
